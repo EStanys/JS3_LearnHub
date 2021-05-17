9c103584fbfe4951aaf885136e18f8027ea3b98d
EStanys <edvinasstanys@gmail.com>
2021-05-17 14:32:37 +0300

    joke function created

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ header.innerHTML = "1990.03.13";
 const bodyEl = document.querySelector("body");
 
 bodyEl.insertAdjacentElement("beforeend", header);
+
+fetch("https://api.chucknorris.io/jokes/random")
+  .then((response) => response.json())
+  .then((data) => jokeHandler(data))
+  .catch((err) => console.warn(err));
+
+function jokeHandler(data) {
+  const jokeEl = document.getElementById("chuck-header");
+  console.log(data);
+
+  const joke = data.value;
+  jokeEl.after(joke);
+}
